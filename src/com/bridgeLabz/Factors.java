d9bdb6e71b2d1b0627d4e6ceba1a0cddad3684c5
Maxen24 <mshubham242@gmail.com>
2022-09-10 22:05:47 +0530

    Added Factors of a Number

---
+++ src/com/bridgeLabz/Factors.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+/**
+ * @author mshub
+ *
+ */
+public class Factors {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int number = 60;
+
+	    System.out.print("Factors of " + number + " are: ");
+
+	    for (int i = 1; i <= number; i++) {
+	      if (number % i == 0) {
+	        System.out.print(i + " ");
+	      }
+	    }
+	  }
+	}
